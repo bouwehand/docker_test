0ab86029bd8ebe762f0d644bfeec01a2930071b0
bouwehand <bouwehand@office.hq.amgate.com>
2016-01-06 11:32:05 +0100

    added error reporting

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 /**
  * Created by PhpStorm.
  * User: bouwehand
@@ -10,3 +12,5 @@ $pdo = new \PDO(
     'demoUser',
     'demoPass'
 );
+
+die(var_dump($pdo));
